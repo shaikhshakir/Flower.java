1285c141d4cbc12c602c7d4f44cb4d16bc02f892
shaikhshakir <87891815+shaikhshakir@users.noreply.github.com>
2021-08-03 10:42:43 -0700

    Add files via upload

---
+++ flower.java
@@ -0,0 +1,25 @@
+class Flower{
+
+void blooming(){
+
+System.out.println(color+""+name+" is blooming...!");
+
+}
+
+void shedding fragrance(){
+
+System.out.println(color+""+name+" is shedding frangrance...!");
+
+}
+
+ public static void main (String args[]){
+ 
+ flower rose=new flower();
+ rose.name="rose";
+ rose.color="red";
+ rose.no of petals=15;
+ rose.blooming();
+ 
+ }
+
+}
